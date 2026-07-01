0c9de36891341696bc5ab4f4506290602739e60c
Karthik Saiharsh <ks.the.visionary@gmail.com>
2026-07-01 17:57:42 +0530

    fix styling of code blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,10 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()]
+    },
+    markdown: {
+        shikiConfig: {
+            theme: 'github-light',
+        }
     }
 });
